72e4a807f8e6e2c0d54f28a2c1a0a5a46459bd6e
Jasper Oudemans <jasperoudemans@gmail.com>
2021-12-21 09:49:10 -0800

    Weather fetch working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,42 @@
 const APIKey = "c5a2aadd2ba954ccd8c6e99df9c362c9";
+
+document.getElementById("checkDatesBtn").addEventListener("click", checkDates);
+
+function checkDates() {
+  const city = document.getElementById("inputCity").value;
+  var queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&units=imperial&appid=" +
+    APIKey;
+
+  fetch(queryURL)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+        console.log(response.json);
+      } else {
+        console.log("Didn't reach");
+        return;
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      console.log(data.coord.lon);
+      console.log(data.coord.lat);
+    });
+}
+
+// fetch(“https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete?query=eiffel%20tower&lang=en_US&units=km”, {
+// 	“method”: “GET”,
+// 	“headers”: {
+// 		“x-rapidapi-host”: “travel-advisor.p.rapidapi.com”,
+// 		“x-rapidapi-key”: “ac0e3c3af6msh7dd67cc95fdd673p1586bbjsn29d733c732e2"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
