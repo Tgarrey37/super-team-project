265fe5bb6c6ddfe0476541e033fbef5e324ffded
Bennet Wilson <bennetwilson@gmail.com>
2021-12-21 12:01:46 -0700

    deleted redundant index element. added to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,13 @@ checkDatesBtn.addEventListener("click", function() {
     hide(searchFields);
     // display(weatherResults);
 })
+
+// weatherResults.innerHTML = `
+// <div class="card mx-auto mt-5" style="width: 18rem;">
+//             <div class="card-body justify-content-center">
+//                 <h5 class="card-title">${}</h5>
+//                 <h6 class="card-subtitle mb-2 text-muted">Currently, it's ${}F.<br> Highs of ${}F.<br> Lows of ${}F.<br> Humidity is ${}%.<br> Wind speed of ${} MPH.<br> Uv is ${}</h6>
+//                 <p class="card-text"> Weather conditions are described as: ${}</p>
+//             </div>
+//         </div>
+// `
