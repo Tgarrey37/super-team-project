81392d3a5cd2f92c1278180f93c527c921fb29c5
Tgarrey37 <tpopp0303@gmail.com>
2021-12-21 11:36:54 -0700

    Resolved js conflict

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,15 @@
-<<<<<<< HEAD
-
-=======
 var searchFields = document.querySelector(".searchFields");
 var checkDatesBtn = document.querySelector("#checkDatesBtn");
 var weatherResults = document.querySelector("");
 
-
 function hide(element) {
-    element.style.display = "none";
+  element.style.display = "none";
 }
 function display(element) {
-    element.style.display = "block";
+  element.style.display = "block";
 }
 
-checkDatesBtn.addEventListener("click", function() {
-    hide(searchFields);
-    // display(weatherResults);
-})
->>>>>>> main
+checkDatesBtn.addEventListener("click", function () {
+  hide(searchFields);
+  // display(weatherResults);
+});
